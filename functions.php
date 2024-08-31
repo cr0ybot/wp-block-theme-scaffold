c6a86503dc57a8f1d20584e8afee7aff72300792
Cory Hughart <cory@coryhughart.com>
2024-08-31 18:36:35 -0500

    update functions file docblock and include autoloader

--- functions.php
+++ functions.php
@@ -1,13 +1,24 @@
 <?php
 /**
- * Theme functions and definitions.
+ * Theme functions file that is autoloaded by WordPress. Used to load required
+ * files and initialize the theme.
  *
- * @package WPBTS
+ * @author    Cory Hughart <cory@coryhughart.com>
+ * @copyright 2024 Cory Hughart
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
+ * @link      https://github.com/cr0ybot/wp-block-theme-scaffold
+ * @package   WPBTS
  */
 
 use WPBTS\Core;
 
-/**
- * Initialize theme.
- */
+// Prevent direct access.
+defined( 'ABSPATH' ) || exit;
+
+// Load Composer autoloader if not already loaded.
+if ( ! class_exists( Core::class ) && is_file( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+// Initialize the core theme class.
 Core::initialize();
