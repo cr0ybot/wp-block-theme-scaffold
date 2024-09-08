b5d50c130c6f7e763828f35b5c4ba4e0d927f2be
Cory Hughart <cory@coryhughart.com>
2024-09-08 10:53:25 -0500

    wp-reset include

---
+++ includes/wp-reset.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Include: wp-reset.php
+ *
+ * Removes default WordPress functionality that is not needed for client projects.
+ * Inspired by 10up's wp-scaffold, @see https://github.com/10up/wp-scaffold/blob/88d4ecada6c3483ce779d55ed1aa2f2191a80d33/themes/10up-theme/includes/overrides.php
+ *
+ * @author    Cory Hughart <cory@coryhughart.com>
+ * @copyright 2024 Cory Hughart
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
+ * @link      https://github.com/cr0ybot/wp-block-theme-scaffold
+ * @package   WPBTS
+ */
+
+// Remove Emoji functionality.
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+add_filter( 'tiny_mce_plugins', __NAMESPACE__ . '\disable_emojis_tinymce' );
+add_filter( 'wp_resource_hints', __NAMESPACE__ . '\disable_emoji_dns_prefetch', 10, 2 );
+
+// Remove WordPress generator meta.
+remove_action( 'wp_head', 'wp_generator' );
+// Remove Windows Live Writer manifest link.
+remove_action( 'wp_head', 'wlwmanifest_link' );
+// Remove the link to Really Simple Discovery service endpoint.
+remove_action( 'wp_head', 'rsd_link' );
+
+
+/**
+ * Filter function used to remove the TinyMCE emoji plugin.
+ *
+ * @link https://developer.wordpress.org/reference/hooks/tiny_mce_plugins/
+ *
+ * @param  array $plugins An array of default TinyMCE plugins.
+ * @return array          An array of TinyMCE plugins, without wpemoji.
+ */
+function disable_emojis_tinymce( $plugins ) {
+	if ( is_array( $plugins ) && in_array( 'wpemoji', $plugins, true ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	}
+
+	return $plugins;
+}
+
+/**
+ * Remove emoji CDN hostname from DNS prefetching hints.
+ *
+ * @link https://developer.wordpress.org/reference/hooks/emoji_svg_url/
+ *
+ * @param  array  $urls          URLs to print for resource hints.
+ * @param  string $relation_type The relation type the URLs are printed for.
+ * @return array                 Difference betwen the two arrays.
+ */
+function disable_emoji_dns_prefetch( $urls, $relation_type ) {
+	if ( 'dns-prefetch' === $relation_type ) {
+		/** This filter is documented in wp-includes/formatting.php */
+		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+
+		$urls = array_values( array_diff( $urls, array( $emoji_svg_url ) ) );
+	}
+
+	return $urls;
+}
