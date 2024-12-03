2be9a8767116d248e3bc4dfb1481521f24eec2ec
Cory Hughart <cory@coryhughart.com>
2024-12-02 18:42:02 -0600

    speed up custom block registration

--- includes/blocks.php
+++ includes/blocks.php
@@ -20,76 +20,17 @@ add_action( 'init', __NAMESPACE__ . '\\block_stylesheet_overrides' );
 add_action( 'init', __NAMESPACE__ . '\\register_pattern_categories' );
 
 /**
- * Automatically registers all blocks that are located within the includes / blocks directory
+ * Automatically registers all blocks that are located within the custom blocks
+ * directory.
  *
  * @return void
  */
 function register_theme_blocks() {
-	global $pagenow;
-
-	// Register all the blocks in the theme.
 	if ( file_exists( WPBTS_BLOCKS_PATH ) ) {
 		$block_json_files = glob( WPBTS_BLOCKS_PATH . '*/block.json' );
 
-		/**
-		 * Filter the block allowlist. This is an array of block names and the
-		 * post types they should be allowed on. If a block is not in this list,
-		 * it will be allowed on all post types. Note that adding a block name
-		 * with an empty array will prevent it from being registered on all post
-		 * types.
-		 *
-		 * If you want to allow a block only on posts and pages, you would return:
-		 *
-		 * ```
-		 * array(
-		 *  'wpbts/example-block' => array( 'post', 'page' ),
-		 * );
-		 * ```
-		 *
-		 * @param array $block_allowlist The block allowlist.
-		 */
-		$block_allowlist = apply_filters( 'wpbts_block_allowlist', array() );
-
-		// Get the current post type.
-		$typenow = false;
-		if ( is_admin() ) {
-			// phpcs:disable WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-			// phpcs:disable WordPress.Security.NonceVerification
-			if ( 'post-new.php' === $pagenow ) {
-				if ( isset( $_REQUEST['post_type'] ) && post_type_exists( wp_unslash( $_REQUEST['post_type'] ) ) ) {
-					$typenow = wp_unslash( $_REQUEST['post_type'] );
-				}
-			} elseif ( 'post.php' === $pagenow ) {
-				// phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
-				if ( isset( $_GET['post'] ) && isset( $_POST['post_ID'] ) && (int) $_GET['post'] !== (int) $_POST['post_ID'] ) {
-					// Do nothing.
-				} elseif ( isset( $_GET['post'] ) ) {
-					$post_id = (int) $_GET['post'];
-				} elseif ( isset( $_POST['post_ID'] ) ) {
-					$post_id = (int) $_POST['post_ID'];
-				}
-				if ( $post_id ) {
-					$post    = get_post( $post_id );
-					$typenow = $post->post_type;
-				}
-			}
-			// phpcs:enable WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-			// phpcs:enable WordPress.Security.NonceVerification
-		}
-
-		// Auto-register all blocks that were found.
 		foreach ( $block_json_files as $filename ) {
-			$block_folder = dirname( $filename );
-			$metadata     = \wp_json_file_decode( $filename, array( 'associative' => true ) );
-
-			if ( $typenow && ! empty( $block_allowlist ) && array_key_exists( $metadata['name'], $block_allowlist ) ) {
-				// If the block is in the allowlist, but the current post type is not in the allowlist, skip it.
-				if ( ! in_array( $typenow, $block_allowlist[ $metadata['name'] ], true ) ) {
-					continue;
-				}
-			}
-
-			\register_block_type_from_metadata( $block_folder );
+			\register_block_type_from_metadata( $filename );
 		}
 	}
 }
